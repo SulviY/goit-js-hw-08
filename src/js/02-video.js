43d9f41c4a3043f31df6443da8a52dca9251e74c
SulviY <yuliia.stepanova@gmail.com>
2021-11-26 13:31:50 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const startReplay = localStorage.getItem("videoplayer-current-time");
+
+const onTimeUpdate = function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
+};
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+
+player.setCurrentTime(startReplay).then(function(seconds) {
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// const onPlay = function (data) {
+//     const timeInLocalStorage = localStorage.getItem("videoplayer-current-time");
+//     console.log(`Time in local storage: ${timeInLocalStorage}`)
+    
+//     const parsedTime = JSON.parse(timeInLocalStorage)
+//     player.setCurrentTime(parsedTime["seconds"])
+// };
+// player.on('play', onPlay);
+
+// const onTimeUpdate = function (data) {
+//     console.log(data)
+//     localStorage.setItem('videoplayer-current-time', JSON.stringify(data)) 
+// };
+// player.on('timeupdate', onTimeUpdate);
